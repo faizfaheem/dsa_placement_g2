e8ffce0ff593571d358fc36e0a1045b47158aba0
faizfaheem <80815546+faizfaheem@users.noreply.github.com>
2021-07-16 16:06:52 +0530

    Add files via upload

---
+++ simple.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main{
+    vector<int>faz[100];
+    for(int i=0;i<100;i++)
+    {
+        cin>>faiz[i];
+
+    }
+    for(int i=0;i<100;i++)
+    {
+        cout<<faiz[i]<<endl;
+    }
+}
